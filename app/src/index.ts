4d99a251defb1946358935e584c3108837430283
kon2300 <yyaamm.2300@gmail.com>
2022-06-08 07:55:50 +0900

    Add: Create class

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,5 +1,94 @@
-const output = (text: string): string => {
-  return text
+interface typeObject {
+  [key: string]: string
 }
 
-console.log(output('HelloWorld'))
+class ObjectWrapper {
+  /***
+   * 引数のオブジェクトのコピーを this._objに設定
+   */
+  constructor(private _obj: Partial<typeObject>) {
+    this._obj = { ..._obj }
+  }
+
+  /**
+   * this._objのコピーを返却
+   * @return Object
+   */
+  get obj(): Partial<typeObject> {
+    return { ...this._obj }
+  }
+
+  /**
+   * this._obj[key] に valを設定。keyがthis._objに存在しない場合、falseを返却
+   * @param key オブジェクトのキー
+   * @param val オブジェクトの値
+   */
+  set(key: string, val: string): boolean {
+    let result: boolean = false
+    Object.keys(this._obj).forEach((e) => {
+      if (e === key) {
+        this._obj[key] = val
+        return (result = true)
+      }
+    })
+    return result ? true : false
+  }
+
+  /**
+   * 指定したキーの値のコピーを返却
+   * 指定のキーが存在しない場合 undefinedを返却
+   * @param key オブジェクトのキー
+   */
+  get(key: string): string | undefined {
+    const result = Object.entries(this._obj).find((val) => {
+      if (val[0] === key) {
+        return val[1]
+      }
+    })
+    return result ? result[1] : undefined
+  }
+
+  /**
+   * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
+   */
+  findKeys(val: unknown): string[] {
+    const result: string[] = Object.keys(this._obj).filter((key) => {
+      return this._obj[key] === val
+    })
+    return result
+  }
+}
+
+/**
+ * check script
+ * 完成したら、以下のスクリプトがすべてOKになる。
+ */
+const obj1 = { a: '01', b: '02' }
+const wrappedObj1 = new ObjectWrapper(obj1)
+
+if (wrappedObj1.obj.a === '01') {
+  console.log('OK: get obj()')
+} else {
+  console.error('NG: get obj()')
+}
+
+if (wrappedObj1.set('c', '03') === false && wrappedObj1.set('b', '04') === true && wrappedObj1.obj.b === '04') {
+  console.log('OK: set(key, val)')
+} else {
+  console.error('NG: set(key, val)')
+}
+
+if (wrappedObj1.get('b') === '04' && wrappedObj1.get('c') === undefined) {
+  console.log('OK: get(key)')
+} else {
+  console.error('NG: get(key)')
+}
+
+const obj2 = { a: '01', b: '02', bb: '02', bbb: '02' }
+const wrappedObj2 = new ObjectWrapper(obj2)
+const keys = wrappedObj2.findKeys('02')
+if (wrappedObj2.findKeys('03').length === 0 && keys.includes('b') && keys.includes('bb') && keys.includes('bbb') && keys.length === 3) {
+  console.log('OK: findKeys(val)')
+} else {
+  console.error('NG: findKeys(val)')
+}
